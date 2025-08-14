189230850ed099b89baf11a36f88fc50b3b600c9
LSJ <lsclsj2000@gmail.com>
2025-08-14 17:03:19 +0900

    [outpolic-LSJ] 초기 세팅 detail : 초기 세팅 / 어플리케이션 프로퍼티스 이그노어 추가

---
+++ ganadinote/src/main/java/ganadinote/GanadinoteApplication.java
@@ -0,0 +1,13 @@
+package ganadinote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanadinoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanadinoteApplication.class, args);
+	}
+
+}
