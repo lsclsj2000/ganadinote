89cda73ddc40e07a0079caa36399b61c6649008c
sepal <sepal@DESKTOP-HURAQ4I>
2025-08-26 19:42:44 +0900

    [ganadinote-ssy] 커뮤니티 메인뷰와 글 디테일뷰 데이터 연결 [상세 내용] 일단 데이터 연결만 해둠. 이제 카테고리 누르면 해당하는 카테고리 글 보이게 하는거랑, 글 더보기 누르면 10개씩 더 볼 수 있게 하는거랑, 댓글 대댓글 작성, 삭제 기능이랑 아무튼 이것저것 더 할 예정

---
+++ ganadinote/src/main/java/ganadinote/community/dto/PostListDTO.java
@@ -0,0 +1,21 @@
+package ganadinote.community.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class PostListDTO {
+	// POSTLIST를 위한 DTO. cmtCount는 댓글 갯수 센거
+	private String categoryNm; 
+	private String postTtl;  
+	private String postCtnt; 
+	private String mbrNknm; 
+	private int postId; 
+	private LocalDateTime postRegDate; 
+	private int postLikeCount; 
+	private int postViewCount; 
+	private int cmtCount;
+
+
+}
